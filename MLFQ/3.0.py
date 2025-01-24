7073e10f38bc097bf53c1a06f1ba3a9e87d8a516
Kiarash Dadpour <140727992+KiarashDadpour@users.noreply.github.com>
2025-01-24 20:01:03 +0330

    Update 3.0.py

--- MLFQ/3.0.py
+++ MLFQ/3.0.py
@@ -1,4 +1,5 @@
 from collections import deque
+import matplotlib.pyplot as plt
 
 def average(lst):
     return sum(lst) / len(lst)
@@ -109,6 +110,38 @@ def mlfq_scheduler(p, at, cbt, cs=1, first_queue_algo='RR', second_queue_algo='R
     return WT, TT, RT, timeline
 
 
+
+def plot_gantt_mlfq(timeline, cs):
+    fig, ax = plt.subplots(figsize=(10, 6))
+    half_cs = cs / 2
+
+    for i, process in enumerate(timeline):
+        index, start, end = process
+        y_pos = index * 1.47
+        ax.broken_barh([(start, end - start)], (y_pos - 0.4, 0.8), facecolors='tab:blue')
+        if cs > 0 and i < len(timeline) - 1:
+            cs_start = end
+            ax.broken_barh([(cs_start, half_cs)], (y_pos - 0.4, 0.8), facecolors='tab:red', alpha=0.5)
+        if cs > 0 and i < len(timeline) - 1:
+            next_start = timeline[i + 1][1]
+            cs_start_next = next_start - half_cs
+            ax.broken_barh([(cs_start_next, half_cs)], ((timeline[i + 1][0] * 1.47) - 0.4, 0.8), facecolors='tab:orange', alpha=0.5)
+    if cs > 0:
+        last_process_end = timeline[-1][2]
+        ax.broken_barh([(last_process_end, half_cs)], (y_pos - 0.4, 0.8), facecolors='tab:green', alpha=0.5)
+
+    ax.set_ylim(0, len(timeline))
+    ax.set_xlim(0, max(t[2] for t in timeline) + cs)
+    ax.set_xlabel("Time")
+    ax.set_ylabel("Processes")
+    ax.set_yticks([t[0] * 1.59 for t in timeline])
+    ax.set_yticklabels([f"P{t[0]}" for t in timeline])
+    ax.grid(True)
+    max_time = max(t[2] for t in timeline) + cs
+    ax.set_xticks(range(0, max_time + 1, 2))
+    plt.title("MLFQ")
+    plt.show()
+
 # Test the scheduler with specific algorithms
 p = 5
 at = [0, 1, 2, 3, 4]
@@ -132,3 +165,5 @@ rtbar = average(c)
 print("Average of WT: ", wtbar)
 print("Average of TT: ", ttbar)
 print("Average of RT: ", rtbar)
+
+plot_gantt_mlfq(timeline, cs)
